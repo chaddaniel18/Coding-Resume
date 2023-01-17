5c5273d0f1a2a700b92a7e1e854f8a63feaf8771
chaddaniel18 <chaddaniel18@gmail.com>
2023-01-17 14:22:02 -0700

    Transfer for continued work on another computer

---
+++ fas-to-phylip.py
@@ -0,0 +1,53 @@
+#  input file location for conversion from fasta (.fas) to phylip (.txt or .phy)
+
+#file = input("enter file location and name: ")
+file = ''
+
+if len(file) == 0:
+    file = '/home/chaddaniel18/Desktop/Phylip-data/bioinfomethods1_labs_Lab3,4_sequences_DNA_aligned.fas'
+
+fasta = []
+
+try:
+    with open(file) as f:
+        for line in f:
+            fasta.append((line))
+        print('file location and name valid')
+except:
+    print('invalid file location and name')
+    exit()
+
+phylip = ''
+switch = 'on'
+num_samples = 0
+
+for line in fasta:
+    if line[0] == '>':
+        num_samples += 1
+        phylip = phylip + '\n'
+        switch = 'on'
+        seq_len = 0
+    else:
+        switch = 'off'
+    if switch == 'on':
+        phylip = phylip + line[1:11].strip()
+    elif switch == 'off':
+        temp_dna = []
+        for char in line:
+            seq_len += 1
+            temp_dna.append(char)
+        dna = ''.join(temp_dna)
+        phylip = phylip + dna
+
+
+print(seq_len)
+with open('/home/chaddaniel18/Desktop/Phylip-data/converted-with-py.phy', 'w') as writer:
+    writer.write(str(num_samples) + ' ' + str(len(dna) - 1))
+    writer.write(phylip)
+
+#switch = 'off'
+#phylip = phylip + '/n'
+
+test = 'ATGCAGAGATTCATGTTTAGCCGCGTCGTGGAACATCAACGTCAGATAAGCCGAGGATTTCTCAGTCTTGTACCATCTCTATCTCCCACTGCTGTTCCTGCTATGTCTCGTTTCTTTCCGAAGATTACTGCTTCTGATTCCACTTCCTCGATTCCCTTTTTTACTCCAGACTTCATCAATCCCAAGAAGACTCTTGAAGAGTCCCTTAACAACTTAGAAGGCCTAACATGTAACCAAGCCGAAAGAGAGATGTATCTCTTTCCACAGATTAATCAACAACGCCTTCTCAACACCACTGGTTCTCGCTTCGGACAGGTTCTTGGAACTTGGCAGTTCAGATGCACAATTCTTCCGGCGAGAGTGAATCGTGTGAGAGAGGTCCACGAGACTTCCAACAACGAAAAGAAACAACAAAAACAAAAAAGTTCCGTCAACGAAAAGAAACCAAAAAAAAAAAAAAAGAGTTCCATCAGCGATATCCCAAGAAGAACAAAGT--TTCAGAAACATCATCGAGGAAGAATTAAT------AAAGGAGTATCTTCTCAGGGGTATATT------TGTAGTAGATATGCTCTTCAAACACTT---GAACCAGCTTGGATCACTTCTAGACAAATAGAAGCAGGACGACGAGCAATGACACGAAATATAGGAC------GTGGTTTAACTGTTCGAGTTCATATATTTGCAGACAAACCAGTTACAGTAAGACCTCCTGAAACGCGTATGGGTCGTGGGAAAGGAGCTCCAGCGTTTTGGGTAGCTGTGGTTAAACCAGGTAAAATCATTTATGAAATGGGTGGTG------TTTCCGAAAAAGTAGCTAGAGAAGCTATTTCTATAGCCGCATCAAAGTTGCCTGCAAAAACCAAATTCATCATTTCTAAATAA------------------------------------------------------------------------------------------------------------------------------------------------------'
+print('test')
+print(len(test))
